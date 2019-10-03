872060acfccc157729d055c196b7a0d74d8c2d60
Johan Lindberg <johan.lindberg@bzzt.se>
2019-10-03 22:07:57 +0200

    Cleaning up a little bit.

--- scrape.py
+++ scrape.py
@@ -24,55 +24,6 @@ def extract(soup):
     ## rmss_c-schedule-game__element.
     games = soup.find_all("div", class_ = "rmss_c-schedule-game__element")
     for game in games:
-        """
-        This is the list of stuff in <game>:
-        <div class="rmss_c-schedule-game__element is-first is-played">
-        <div class="rmss_c-schedule-game__wrapper">
-        <div class="rmss_c-schedule-game__info-wrapper">
-        <div class="rmss_c-schedule-game__team is-home-team">
-        <div class="rmss_c-schedule-game__team-logo"><img src="/team_graphics/nef_da/kun1-kun-e2357/logo_square/60.png" srcset="/team_graphics/nef_da/kun1-kun-e2357/logo_square/90.png 1.5x, /team_graphics/nef_da/kun1-kun-e2357/logo_square/120.png 2x"/></div>
-        <div class="rmss_c-schedule-game__team-name">Kungsbacka</div>
-        <div class="rmss_c-schedule-game__team-name rmss_c--mobile">KUN</div>
-        </div>
-        <div class="rmss_c-schedule-game__result is-gallery-layout">
-        <span class="rmss_c-schedule-game__team-result is-home-team">0</span>
-        <span class="rmss_c-schedule-game__divider">-</span>
-        <span class="rmss_c-schedule-game__team-result is-winner is-away-team">3</span>
-        </div>
-        <div class="rmss_c-schedule-game__center-text">
-        <div class="rmss_c-schedule-game__result is-list-layout">
-        <span class="rmss_c-schedule-game__team-result is-home-team">0</span>
-        <span class="rmss_c-schedule-game__divider">-</span>
-        <span class="rmss_c-schedule-game__team-result is-winner is-away-team">3</span>
-        </div>
-        </div>
-        <div class="rmss_c-schedule-game__team is-away-team">
-        <div class="rmss_c-schedule-game__team-logo"><img src="/team_graphics/nef_da/kop1-kop-13892/logo_square/60.png" srcset="/team_graphics/nef_da/kop1-kop-13892/logo_square/90.png 1.5x, /team_graphics/nef_da/kop1-kop-13892/logo_square/120.png 2x"/></div>
-        <div class="rmss_c-schedule-game__team-name">Kopparbergs / Göteborg</div>
-        <div class="rmss_c-schedule-game__team-name rmss_c--mobile">KOP</div>
-        </div>
-        <div class="rmss_c-schedule-game__info">
-        <span class="rmss_c-schedule-game__live-info rmss_c--mobile">Slut</span>
-        <span class="rmss_c-schedule-game__info__round-number">Omgång 1</span>
-        </div>
-        </div>
-        <div class="rmss_c-schedule-game__live-info-wrapper">
-        <div class="rmss_c-schedule-game__live-info">Slut</div>
-        <i class="fa fa-plus rmss_c-schedule-game__extra-button-icon"></i>
-        <div class="rmss_c-schedule-game__extra-button" data-game-extra="KUN_KOP_2019-04-13"></div>
-        </div>
-        </div>
-        <div class="rmss_c-schedule-game__extra is-hidden" data-game-extra="KUN_KOP_2019-04-13">
-        <div class="rmss_c-schedule-game__extra-link rmss_c--arena">
-        <a href="/gamecenter/qWY-9YJN3v8eQ/articles/post">
-        <span class="rmss_c-schedule-game__extra-link__wrapper">
-        <span class="rmss_c-schedule-game__extra-link__text">Efter match</span>
-        </span>
-        </a>
-        </div>
-        </div>
-        </div>
-        """
         round_ = game.find("span", class_ = "rmss_c-schedule-game__info__round-number").text
         date_ = game.find("div", class_ = "rmss_c-schedule-game__extra-button")['data-game-extra'].split('_')[-1]
         home = game.find("div", class_ = "rmss_c-schedule-game__team is-home-team").text.strip().replace('\n', ' ')
