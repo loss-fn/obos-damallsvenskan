f441bb9eafc032e91c2103680eb6ec029efd2c05
Johan Lindberg <johan.lindberg@bzzt.se>
2019-10-03 11:45:30 +0200

    Adding scrape.py as a way to get data from OBOS Damallasvenskan web site.

---
+++ scrape.py
@@ -0,0 +1,11 @@
+## Predicting OBOS Damallsvenskan 2019 results
+## -------------------------------------------
+
+## scrape.py is used to get the match data from OBOS Damallsvenskan
+## web site using requests and Beautiful Soup.
+
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    pass
