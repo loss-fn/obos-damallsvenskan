a1d0a12ae6507d6b5b21bb50de313780ef191265
Johan Lindberg <johan.lindberg@multipliers.se>
2019-10-07 10:54:19 +0200

    Added more classifiers and return the average result.
    We did some analysis and found out that the score varies from 6-7 to 13-15 when
    we use a single RFClassifier. So we thought if we generate a 100 and let them
    predict a 100 times each and take the average prediction it should smooth things
    out. Which it did. The average error is now between 8 and 9.

--- first_attempt.py
+++ first_attempt.py
@@ -1,3 +1,5 @@
+import statistics
+
 import numpy as np
 import pandas as pd
 
@@ -16,21 +18,43 @@ def test_data(df):
     y = df.iloc[108:114, -2:].to_numpy()
     return x, y
 
-def make_classifier(xTrain, yTrain):
-    clf = RandomForestClassifier(n_estimators = 10)
-    clf.fit(xTrain, yTrain)
-    return clf
+def make_classifiers(xTrain, yTrain):
+    from random import randint
+    result = []
+    for _ in range(100):
+        clf = RandomForestClassifier(n_estimators = randint(8,32))
+        clf.fit(xTrain, yTrain)
+        result.append(clf)
+    return result
 
-def test_classifier(clf, xTest, yTest):
-    pTest = clf.predict(xTest)
-    return pTest, score_prediction(pTest, yTest)
+def test_classifiers(clfs, xTest, yTest):
+    result = []
+    for _ in range(100):
+        for clf in clfs:
+            pTest = clf.predict(xTest)
+            result.append(score_prediction(pTest, yTest))
+
+    return statistics.mean(result)
     
 def score_prediction(pTest, yTest):
     pDiff = yTest[:,:]-pTest
     return sum(sum(pDiff))
 
+def predict(clfs, xTest):
+    result =[] 
+    for clf in clfs:
+        pTest = clf.predict(xTest)
+        result.append(pTest)
+
+    result = np.asarray(result)
+    return result[:,:].mean(axis = 0).round()
+
 if __name__ == "__main__":
     xTrain, yTrain = training_data(load_('train.csv'))
     xTest, yTest = test_data(load_('train.csv'))
-    clf = make_classifier(xTrain, yTrain)
-    print(test_classifier(clf, xTest, yTest))
+    clfs = make_classifiers(xTrain, yTrain)
+    #print(test_classifiers(clfs, xTest, yTest))
+    pTest = predict(clfs, xTest)
+    print(yTest)
+    print(pTest)
+    print(score_prediction(pTest, yTest))
